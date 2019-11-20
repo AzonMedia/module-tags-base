87680255e5a01680142a3245e1d8b518bc328fb7
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-20 17:28:45 +0200

    added comments

--- src/Interfaces/TagInterface.php
+++ src/Interfaces/TagInterface.php
@@ -5,7 +5,16 @@ namespace GuzabaPlatofrm\Tags\Base\Interfaces;
 
 interface TagInterface
 {
+    /**
+     * Returns the tag name.
+     * @return string
+     */
     public function get_name() : string ;
 
+    /**
+     * Creates a new tag and returns the newly created object.
+     * @param string $tag_name
+     * @return object
+     */
     public static function create(string $tag_name) : object ;
 }
